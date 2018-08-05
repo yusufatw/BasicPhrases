6d1391bb4ecc5bc82d2a295210c1a158724704c2
yusufatw <yusufatw@gmail.com>
2018-08-05 13:07:09 +0700

    Initial commit

---
+++ app/src/main/java/id/cathode/basicphrases/MainActivity.java
@@ -0,0 +1,31 @@
+package id.cathode.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "info ";
+
+    public void buttonTapped(View view){
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw","id.cathode.basicphrases");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+
+        Log.i(TAG, ourId);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
